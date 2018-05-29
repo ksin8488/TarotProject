f25fdd6b8e38a49751fbe98c81521cd6aba2ea82
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-05-29 12:41:11 -0600

    Created a new class for creating another panel to use in the GUI.
    The idea will be that there will be a main menu screen with options to create different layouts of TarotCard positions with other panels. I may need to create new panel classes for each one or there may be an easier way in the future.

---
+++ src/tarot/view/MainMenuPanel.java
@@ -0,0 +1,24 @@
+package tarot.view;
+
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+
+import tarot.controller.TarotController;
+import tarot.model.TarotCards;
+import tarot.model.Card;
+import java.awt.Color;
+import javax.swing.SpringLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainMenuPanel extends JPanel
+{
+	
+}
